0f8e7ce8b9c4f01bbe1f2eed9c5b9f748475036c
Muhammad Yasin Habiburrahman <abikdanebo@gmail.com>
2026-05-12 10:28:24 +0700

    Praktikum Struktur Data Pekan 1

--- PrakSD2026_B_2511532016/src/pekan1_2511532016/JamDriver_2511532016.java
+++ PrakSD2026_B_2511532016/src/pekan1_2511532016/JamDriver_2511532016.java
@@ -1,16 +1,12 @@
-package pekan1_2511532016;
-
+package pekan1_2511532016;																		
 public class JamDriver_2511532016 {
 	public static void main(String[] args) {
-		Jam_2511532016 a = new Jam_2511532016(23, 59, 50);
-		Jam_2511532016 b = new Jam_2511532016(00, 00, 15);
-		System.out.println("a               = " + a);
-		System.out.println("b               = " + b);
-		System.out.println("a+b             = " + a.plus(b));
-		System.out.println("next 20s        = " + a.nextNSecond(20));
-		System.out.println("durasi(a,b)     = " + Jam_2511532016.durasiDetik(a, b) );
-		System.out.println("a.compareTo(b)  = " + a.compareTo(b));
-		
-	}
-
-}
+		Jam_2511532016 a_2016 = new Jam_2511532016(23, 59, 50);
+		Jam_2511532016 b_2016 = new Jam_2511532016(00, 00, 15);
+		System.out.println("a               = " + a_2016);
+		System.out.println("b               = " + b_2016);
+		System.out.println("a+b             = " + a_2016.plus(b_2016));
+		System.out.println("next 20s        = " + a_2016.nextNSecond(20));
+		System.out.println("durasi(a,b)     = " + Jam_2511532016.durasiDetik(a_2016,b_2016));
+		System.out.println("a.compareTo(b)  = " + a_2016.compareTo(b_2016));
+	}}	
